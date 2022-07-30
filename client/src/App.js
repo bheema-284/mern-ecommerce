e59dd717743bc1ac5b97b24459666e1e73713d8b
Bheemudu Guguloth <96168073+bheema-284@users.noreply.github.com>
2022-07-30 10:52:08 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const App = () => <div>Hello from Bheemudu Guguloth</div>; 
+const App = () => <div>
+      Hello from Bheemudu Guguloth
+        </div>; 
 
 export default App;
